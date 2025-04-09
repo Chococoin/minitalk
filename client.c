a2f83f95a37ffa98c659134c43ec8217a5658a4e
German Lugo <g.lugo.dev@gmail.com>
2025-04-09 21:41:57 +0200

    Initial commit.

---
+++ client.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: glugo-mu <glugo-mu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/08 22:13:53 by glugo-mu          #+#    #+#             */
+/*   Updated: 2025/04/09 21:29:45 by glugo-mu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void	send_char(pid_t pid, unsigned char c)
+{
+	int	i;
+
+	i = 7;
+	while (i >= 0)
+	{
+		if ((c >> i) & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(100);
+		i--;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	pid_t	pid;
+	char	*str;
+	int		i;
+
+	if (argc != 3)
+		return (1);
+	pid = (pid_t)atoi(argv[1]);
+	str = argv[2];
+	i = 0;
+	while (str[i])
+	{
+		send_char(pid, str[i]);
+		i++;
+	}
+	send_char(pid, '\0');
+	return (0);
+}
+
---
+++ server.c
@@ -0,0 +1,75 @@
+#include <unistd.h>
+#include <signal.h>
+
+void	handler(int signum, siginfo_t *info, void *context)
+{
+	static int				bit_count;
+	static unsigned char	char_accum;
+
+	(void)context;
+	if (!info || info->si_pid <= 0)
+		return;
+	if (signum != SIGUSR1 && signum != SIGUSR2)
+		return;
+	char_accum <<= 1;
+	if (signum == SIGUSR1)
+		char_accum |= 1;
+	if (++bit_count == 8)
+	{
+		if (char_accum == 0)
+		{
+			write(1, "\n[FIN]\n", 7);
+			return;
+		}
+		else
+			write(1, &char_accum, 1);
+		bit_count = 0;
+		char_accum = 0;
+	}
+}
+
+void	ft_putnbr(int num)
+{
+	char	n;
+
+	if (num < 0)
+	{
+		write(1, "-", 1);
+		num = -num;
+	}
+	if (num >= 10)
+		ft_putnbr(num / 10);
+	n = num % 10 + '0';
+	write(1, &n, 1);
+}
+
+int	ft_strlen(char *s)
+{
+	int	len;
+
+	len = 0;
+	while (*s++)
+		len++;
+	return (len);
+}
+
+int	main(void)
+{
+	int					pid;
+	struct sigaction	sa;
+
+	pid = getpid();
+	write(1, "PID: ", 5);
+	ft_putnbr(pid);
+	write(1, "\n", 1);
+	sa.sa_sigaction = &handler;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	sigaddset(&sa.sa_mask, SIGUSR1);
+	sigaddset(&sa.sa_mask, SIGUSR2);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
+	return (0);
+}
