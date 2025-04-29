e64fd0d4df806da291ec3dc3d77d2f18c0a285b0
German G. Lugo Munoz <glugo-mu@42firenze.it>
2025-04-29 20:19:10 +0200

    Heavy refactor.

--- client.c
+++ client.c
@@ -6,67 +6,67 @@
 /*   By: glugo-mu <glugo-mu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 22:13:53 by glugo-mu          #+#    #+#             */
-/*   Updated: 2025/04/28 13:29:10 by glugo-mu         ###   ########.fr       */
+/*   Updated: 2025/04/29 20:13:46 by glugo-mu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-static volatile sig_atomic_t	g_ack;
-
-void	handler(int signum, siginfo_t *info, void *ctx)
+void	handler(int signum)
 {
-	(void)ctx;
-	(void)info;
-	(void)signum;
-	if (signum != SIGUSR1)
-		return ;
-	g_ack = 1;
+	if (signum == SIGUSR1)
+		write(1, "Bit has been receieved\n", 23);
+	else if (signum == SIGUSR2)
+		write(1, "Character has been delivered\n", 29);
 }
 
-void	send_char(pid_t pid, unsigned char c)
-{
-	int	i;
+void	config_signals(void)
+{	
+	struct sigaction	sa;
 
-	i = 7;
-	g_ack = 1;
-	while (i >= 0)
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_handler = &handler;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+}
+
+void	send_str(pid_t pid, char *s)
+{
+	unsigned char	c;
+	int				nbr_bits;
+
+	while (*s)
 	{
-		if ((c >> i) & 1)
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR2);
-		i--;
-		g_ack = 0;
-		while (!g_ack)
-			pause();
+		c = *s;
+		nbr_bits = 8;
+		while (nbr_bits--)
+		{
+			if ((c >> 7) & 1)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			usleep(100);
+			c <<= 1;
+		}
+		s++;
 	}
+	exit(1);
 }
 
 int	main(int argc, char **argv)
 {
 	pid_t				pid;
-	char				*str;
-	int					i;
-	struct sigaction	sa;
 
-	sa.sa_flags = SA_SIGINFO;
-	sa.sa_sigaction = handler;
-	sigemptyset(&sa.sa_mask);
-	sigaction(SIGUSR1, &sa, NULL);
 	if (argc != 3)
 	{
 		write(2, "Uso: ./client [PID_SERVER] [MENSSAGE]\n", 38);
 		return (1);
 	}
 	pid = (pid_t)ft_atoi(argv[1]);
-	str = argv[2];
-	i = 0;
-	while (str[i])
-	{
-		send_char(pid, str[i]);
-		i++;
-	}
-	send_char(pid, '\0');
+	config_signals();
+	send_str(pid, argv[2]);
+	while (1)
+		pause();
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: glugo-mu <glugo-mu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 22:05:13 by glugo-mu          #+#    #+#             */
-/*   Updated: 2025/04/16 14:19:59 by glugo-mu         ###   ########.fr       */
+/*   Updated: 2025/04/29 19:46:37 by glugo-mu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,49 +14,50 @@
 
 void	handler(int signum, siginfo_t *info, void *ctx)
 {
-	static int				bit_count;
+	static int				bit_count = -1;
 	static unsigned char	char_accum;
 
 	(void)ctx;
-	if (!info || info->si_pid <= 0)
-		return ;
-	if (signum != SIGUSR1 && signum != SIGUSR2)
-		return ;
-	char_accum <<= 1;
+	if (bit_count < 0)
+		bit_count = 7;
 	if (signum == SIGUSR1)
-		char_accum |= 1;
-	if (++bit_count == 8)
+		char_accum |= (1 << bit_count);
+	bit_count--;
+	if (bit_count < 0)
 	{
-		if (char_accum == 0)
-			write(1, "\n[FIN]\n", 7);
-		else
-			write(1, &char_accum, 1);
-		bit_count = 0;
+		write(1, &char_accum, 1);
 		char_accum = 0;
+		if (kill(info->si_pid, SIGUSR2) == -1)
+			write(2, "Error: Failed to send SIGUSR2\n", 30);
+		return ;
 	}
-	usleep(10);
-	kill(info->si_pid, SIGUSR1);
+	if (kill(info->si_pid, SIGUSR1) == -1)
+		write(2, "Error: Failed to send SIGUSR1\n", 30);
+}
+
+void	set_signals(void)
+{
+	struct sigaction	sa;
+
+	sa.sa_sigaction = &handler;
+	sa.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+		exit(1);
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+		exit(1);
 }
 
 int	main(void)
 {
 	int					pid;
-	struct sigaction	sa;
 
 	pid = getpid();
 	write(1, "PID: ", 5);
 	ft_putnbr(pid);
+	write(1, "\n", 1);
 	if (pid <= 0)
 		return (1);
-	write(1, "\n", 1);
-	sa.sa_sigaction = &handler;
-	sa.sa_flags = SA_SIGINFO;
-	sigemptyset(&sa.sa_mask);
-	sigaddset(&sa.sa_mask, SIGUSR1);
-	sigaddset(&sa.sa_mask, SIGUSR2);
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
 	while (1)
-		pause();
+		set_signals();
 	return (0);
 }
